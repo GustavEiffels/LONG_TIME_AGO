a288d84261301cb0d4c114799c6d6ca387f6663b
singsi <sibuk9315@gmail.com>
2022-03-04 12:13:54 +0900

    nin

---
+++ app/src/main/java/com/sing/twice_First/Sana4.kt
@@ -0,0 +1,11 @@
+package com.sing.twice_First
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Sana4 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sana4)
+    }
+}
