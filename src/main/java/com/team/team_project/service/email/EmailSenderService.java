a084476ac5003413554a3bd50c5a5db36afa0d6d
singsi <sibuk9315@gmail.com>
2022-03-12 18:56:31 +0900

    modifyFindFunction

--- src/main/java/com/team/team_project/service/email/EmailSenderService.java
+++ src/main/java/com/team/team_project/service/email/EmailSenderService.java
@@ -11,9 +11,9 @@ import java.util.Properties;
 @Service
 public class EmailSenderService {
     public void sendMail(String title, String sendTo, String content) throws MessagingException {
-        final String user = "singsiuk9315@naver.com";
-        final String password = "illsuccess5950";
-        final String sender = "singsiuk9315@naver.com";
+        final String user = "";
+        final String password = "";
+        final String sender =;
         String smtpMailLocate = "smtp.naver.com";
 
         Properties prop = new Properties();
