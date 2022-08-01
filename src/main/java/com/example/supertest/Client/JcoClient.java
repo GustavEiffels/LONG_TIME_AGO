eca171fb0919e5d8f50b7233c518e2ad5518ed1c
Yuhyun <yuhyun.park@sobetec.com>
2022-08-01 16:57:01 +0900

    rollback

--- src/main/java/com/example/supertest/Client/JcoClient.java
+++ src/main/java/com/example/supertest/Client/JcoClient.java
@@ -2,15 +2,9 @@ package com.example.supertest.Client;
 
 import java.io.File;
 import java.io.FileOutputStream;
-import java.security.Key;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 import java.util.Properties;
 
 import com.example.supertest.Wrapper.Company_Info__c;
-import com.example.supertest.Wrapper.Company_List__c;
 import com.google.gson.Gson;
 import com.sap.conn.jco.AbapException;
 import com.sap.conn.jco.JCoDestination;
@@ -19,14 +13,13 @@ import com.sap.conn.jco.JCoException;
 import com.sap.conn.jco.JCoFunction;
 import com.sap.conn.jco.JCoMetaData;
 import com.sap.conn.jco.JCoStructure;
-import com.sap.conn.jco.JCoTable;
 import com.sap.conn.jco.ext.DestinationDataProvider;
 
 public class JcoClient {
     static String ABAP_AS = "ABAP_AS_WITHOUT_POOL";
     static String ABAP_AS_POOLED = "ABAP_AS_WITH_POOL";
     static String ABAP_MS = "ABAP_MS_WITHOUT_POOL"; // Use Message Server
-
+ 
     // Properties 설정
     static {
         Properties connectProperties = new Properties();
@@ -37,13 +30,13 @@ public class JcoClient {
         connectProperties.setProperty(DestinationDataProvider.JCO_PASSWD, "thqpxpr2022~"); // 암호
         connectProperties.setProperty(DestinationDataProvider.JCO_LANG, "KO"); // 언어
         createDestinationDataFile(ABAP_AS, connectProperties);
-
+ 
         connectProperties.setProperty(DestinationDataProvider.JCO_POOL_CAPACITY, "3"); // 대상에서 열린 상태로 유지되는 최대 유휴 연결
-                                                                                       // 개수입니다. Default = 1
-
+                                                                                        // 개수입니다. Default = 1
+ 
         connectProperties.setProperty(DestinationDataProvider.JCO_PEAK_LIMIT, "10"); // 대상에 대해 동시에 만들 수 있는 최대 활성 연결
-                                                                                     // 개수입니다. Default = 0(무제한)
-
+                                                                                        // 개수입니다. Default = 0(무제한)
+ 
         createDestinationDataFile(ABAP_AS_POOLED, connectProperties);
     }
 
@@ -59,7 +52,6 @@ public class JcoClient {
         }
     }
 
-    // 회사코드 입력하여 특정 회사코드만 import
     public static Company_Info__c getCompany(String id) throws JCoException {
         JCoDestination destination = JCoDestinationManager.getDestination(ABAP_AS_POOLED);
         JCoFunction function = destination.getRepository().getFunction("BAPI_COMPANY_GETDETAIL");
@@ -73,12 +65,12 @@ public class JcoClient {
             return null;
         }
         JCoStructure detail = function.getExportParameterList().getStructure("COMPANY_DETAIL");
-        JCoMetaData table = detail.getMetaData();
+        JCoMetaData detailMeta = detail.getMetaData();
         Company_Info__c company = new Company_Info__c();
         System.out.println(detail.toString());
-        for (int i = 0; i < table.getFieldCount(); i++) {
+        for(int i = 0; i < detailMeta.getFieldCount(); i++) {
             try {
-                company.setField(table.getName(i), detail.getString(table.getName(i)));
+                company.setField(detailMeta.getName(i), detail.getString(detailMeta.getName(i)));
             } catch (Exception e) {
                 System.out.println(e.toString());
             }
@@ -86,40 +78,6 @@ public class JcoClient {
         return company;
     }
 
-    // BAPI_COMPANY_GETLIST : import parameter 값 없이 모든 리스트 import
-    public static Map<String, Object> getCompanyList() throws JCoException {
-        Map<String, Object> resultMap = new HashMap<String, Object>();
-        JCoDestination destination = JCoDestinationManager.getDestination(ABAP_AS_POOLED);
-        JCoFunction function = destination.getRepository().getFunction("BAPI_COMPANY_GETLIST");
-        if (function == null)
-            throw new RuntimeException("Function을 찾을 수 없습니다.");
-        try {
-            function.execute(destination);
-        } catch (AbapException e) {
-            System.out.println(e.toString());
-            return null;
-        }
-
-        // JCoTable table = function.getExportParameterList().getTable("COMPANY_LIST");
-        // JCoMetaData tableMeta = table.getMetaData();
-        // Company_List__c companyList = new Company_List__c();
-
-        // Map<String, Map<String, String>> exportParamList = new HashMap<>();
-        // for(int i = 0; i < function.getExportParameterList().getFieldCount(); i++;){
-        //     JCoStructure structure = function.getExportParameterList().getStructure(arg0)
-        // }
-
-        List<String> tableParamList = new ArrayList<>();
-        try{
-            for(int i = 0; i < function.getTableParameterList().getFieldCount(); i++){
-                tableParamList.add(function.getTableParameterList().getTable(i).getMetaData().toString());
-            }
-        }catch(Exception e){
-
-        }
-        resultMap.put("table", tableParamList);
-        return resultMap;
-    }
     public static void main(String[] args) {
         Gson gson = new Gson();
         try {
